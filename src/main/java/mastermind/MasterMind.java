d17b756233ac9c0d6244376aef91594a76d8325e
nbudiman <clipxclop@gmail>
2019-10-03 09:35:18 +0800

    Adds test for perfect match - simple solution

--- src/main/java/mastermind/MasterMind.java
+++ src/main/java/mastermind/MasterMind.java
@@ -15,15 +15,21 @@ public class MasterMind {
 
         List<Character> feedback = new ArrayList<>();
 
+        // TODO - once you have identified that guess[n] == code[n],
+        //        then you must never compare anything to guess[n] or code[n]
+
         for (int guessIndex = 0; guessIndex < guess.length; guessIndex++) {
             for (int codeIndex = 0; codeIndex < code.length; codeIndex++) {
                 if (guess[guessIndex] == code[codeIndex]) {
                     if (guessIndex == codeIndex) {
                         feedback.add('R');
+                        code[codeIndex] = 'X';
+                        break;
                     }
                     else {
                         feedback.add('W');
                     }
+
                 }
 
             }
--- src/test/java/mastermind/MasterMindTest.java
+++ src/test/java/mastermind/MasterMindTest.java
@@ -60,5 +60,37 @@ public class MasterMindTest {
         assertThat(feedback).containsExactly('R');
     }
 
+    @Test
+    public void getsOneWhiteOneRed() {
+
+        // given
+        Character[] code = { 'R', 'B', 'R', 'G' };
+        MasterMind masterMind = new MasterMind(code);
+
+        Character[] guess = { 'Y', 'B', 'G', 'Y' };
+
+        // when
+        Character[] feedback = masterMind.play(guess);
+
+        // then
+        assertThat(feedback).containsExactlyInAnyOrder('W', 'R');
+    }
+
+    @Test
+    public void perfectMatchGetsAllRed() {
+
+        // given
+        Character[] code = { 'B', 'B', 'B', 'B' };
+        MasterMind masterMind = new MasterMind(code);
+
+        Character[] guess = { 'B', 'B', 'B', 'B'  };
+
+        // when
+        Character[] feedback = masterMind.play(guess);
+
+        // then
+        assertThat(feedback).containsExactlyInAnyOrder('R', 'R', 'R', 'R');
+    }
+
 
 }
